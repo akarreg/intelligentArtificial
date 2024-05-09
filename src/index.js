fdc6464d32bd929db562ee47237105e514dc43bd
akarreg <161454874+akarreg@users.noreply.github.com>
2024-05-09 21:19:27 +0200

    java done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function ingredient(event) {
   event.preventDefault();
-  let recipe = document.querySelector("#result");
-  recipe.innerHTML = "hi";
-}
 
+  let recipe = document.querySelector("#result");
+  recipe.innerHTML = "Searching for the best recipe ...";
+}
 let pressButton = document.querySelector("#search");
 pressButton.addEventListener("click", ingredient);
