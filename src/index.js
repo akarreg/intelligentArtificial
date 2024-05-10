f742e3dc742233e6df2a38341b59e8aad4536f4e
akarreg <161454874+akarreg@users.noreply.github.com>
2024-05-10 22:06:17 +0200

    AI add to java

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function displayRecipe(response) {
+  new Typewriter("#result", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
 function ingredient(event) {
   event.preventDefault();
-  new Typewriter("#result", {
-    strings: "Searching for the best recipe....",
-    autoStart: true,
-    delay: 400,
-    cursor: "",
-  });
+  let userProduct = document.querySelector("#ingredient");
+  let apiKey = "bc0c992ff01fe3156bt9ead9dob31418";
+  let prompt = `  Generated a recipe  includng the ${userProduct.value} as a ingredient`;
+
+  let context =
+    "You have the best recipes in the world ,you are an expert in the classic culinary world.";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Searching for the best recipe.....");
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let pressButton = document.querySelector("#search");
